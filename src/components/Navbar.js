2b2e8145f255feb311c3b190c828c0f67c836499
Lehane-P <Lehane-P>
2023-08-23 18:36:15 +0300

    updated Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,25 +3,46 @@
 
 const Navbar = ()  => {
     return(
-        <div className="navbar-container">
-            <div className="navbar-list">
-                <div className="intro"></div>
+        // <div className="navbar-container">
+        //     <div className="navbar-list">
+        //         <div className="intro"></div>
         
         
-            <h4>Home</h4>
-            <h4>Blogs</h4>
-            <h4>Certificates</h4>
-            <h4>Inbox</h4>
+        //     <h4>Home</h4>
+        //     <h4>Blogs</h4>
+        //     <h4>Certificates</h4>
+        //     <h4>Inbox</h4>
             
             
         
-        </div>
-        </div>
-    )
-}
+        // </div>
+        // </div>
+       
+
+        
+       
+        
+            <nav className="navbar">
+              <div className="navbar-list"></div>
+              <ul className="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Blogs</a></li>
+                <li><a href="#">Certificates</a></li>
+                <li><a href="#">Inbox</a></li>
+              </ul>
+            </nav>
+          );
+        };
+        
+        export default Navbar;
+        
 
 
-export default Navbar;
+    
+
+
+
+
 
 // git branch <name> => creates a branch
 // git checkout <name> => 
