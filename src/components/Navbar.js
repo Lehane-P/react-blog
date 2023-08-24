4b347af201922cbfd8bfbfff9097cf3299141a66
muxsinyusuf <mucsin.yusuf@student.moringaschool.com>
2023-08-24 15:37:43 +0300

    making Block

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,18 @@
-
-
-
-const Navbar = ()  => {
-    return(
-        <div className="navbar-container">
-            <div className="navbar-list">
-                <div className="intro"></div>
-        
-        
-            <h4>Home</h4>
-            <h4>Blogs</h4>
-            <h4>Certificates</h4>
-            <h4>Inbox</h4>
-            
-            
-        
-        </div>
-        </div>
-    )
-}
-
+const Navbar = () => {
+  return (
+    <div classname="navbar-container">
+      <div classname="navbar-list">
+        <div classname="intro" />
+        <h4>Home</h4>
+        <h4>Blogs</h4>
+        <h4>Certificates</h4>
+        <h4>Inbox</h4>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
 
 // git branch <name> => creates a branch
-// git checkout <name> => 
+// git checkout <name> =>
